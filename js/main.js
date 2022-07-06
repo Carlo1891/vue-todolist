175a557eff038c2e710e06848ac4a978a383ec73
Carlo1891 <carlo.18@live.it>
2022-07-06 16:25:03 +0200

    Inizio

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// Esercizio di oggi: Vue To Do List
+// nome repo: vue-todolist
+// Descrizione:
+// Rifare l'esercizio della to do list.
+// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+// text, una stringa che indica il testo del todo
+// done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+// E' tassativamente obbligatorio rispettare le milestone nell'ordine in cui sono elencate e fare almeno una commit per ogni milestone: pena la valutazione dell'esercizio.
+// MILESTONE 1
+// Stampare all'interno di una lista, un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+// Bonus:
+// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+// 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+// :stone_da_curling: Buon lavoro e buon divertimento a tutte e a tutti :stone_da_curling:!
+
+// esempio di todos:
+// todos: [
+// 	{
+// 		text: 'Fare i compiti',
+// 		done: false
+// 	},
+// 	{
+// 		text: 'Fare la spesa',
+// 		done: true
+// 	},
+// 	{
+// 		text: 'Fare il bucato',
+// 		done: false
+// 	}
+// ]
